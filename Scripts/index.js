5042d980f8ba85581932c181cf7f234a306005f2
birukshegute <birukshegute@gmail.com>
2024-04-16 14:15:40 +0300

    firebase might be it

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,11 +1,24 @@
-const login = document.getElementById("login");
-const signup = document.getElementById("signup");
-const about = document.getElementById("about");
-const Box = document.getElementById("Box");
-const Add = document.getElementById("Add");
-const pri = document.getElementById("pri");
-const low = document.getElementById("low");
-const medium = document.getElementById("medium");
-const high = document.document.getElementById("high");
+import { initializeApp } from "firebase/app";
 
-console.log('hello')
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD0AyE0wzp7Ga8BUWN_h8_Aj8qG0sLAREg",
+    authDomain: "todolist-aec4c.firebaseapp.com",
+    projectId: "todolist-aec4c",
+    storageBucket: "todolist-aec4c.appspot.com",
+    messagingSenderId: "60401679256",
+    appId: "1:60401679256:web:4026ff726358d9e6d889ed",
+    measurementId: "G-CRTYC1H96P"
+  };
+
+initializeApp(firebaseConfig);
+
+// const login = document.getElementById("login");
+// const signup = document.getElementById("signup");
+// const about = document.getElementById("about");
+// const Box = document.getElementById("Box");
+// const Add = document.getElementById("Add");
+// const pri = document.getElementById("pri");
+// const low = document.getElementById("low");
+// const medium = document.getElementById("medium");
+// const high = document.document.getElementById("high");
