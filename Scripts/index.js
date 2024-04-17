57fce6e2cf1e518f8103fdb8777d2eda234e2422
birukshegute <birukshegute@gmail.com>
2024-04-17 11:39:02 +0300

    I can't for God's sake

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,49 +1,47 @@
-import { initializeApp } from "firebase/app";
-import {
-    getFirestore, collection, getDocs
-} from 'firebase/firestore'
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyD0AyE0wzp7Ga8BUWN_h8_Aj8qG0sLAREg",
-    authDomain: "todolist-aec4c.firebaseapp.com",
-    projectId: "todolist-aec4c",
-    storageBucket: "todolist-aec4c.appspot.com",
-    messagingSenderId: "60401679256",
-    appId: "1:60401679256:web:4026ff726358d9e6d889ed",
-    measurementId: "G-CRTYC1H96P"
-  };
+// import { initializeApp } from "firebase/app";
+// import {
+//     getFirestore, collection, getDocs
+// } from 'firebase/firestore'
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyD0AyE0wzp7Ga8BUWN_h8_Aj8qG0sLAREg",
+//     authDomain: "todolist-aec4c.firebaseapp.com",
+//     projectId: "todolist-aec4c",
+//     storageBucket: "todolist-aec4c.appspot.com",
+//     messagingSenderId: "60401679256",
+//     appId: "1:60401679256:web:4026ff726358d9e6d889ed",
+//     measurementId: "G-CRTYC1H96P"
+//   };
 
-//initialize firebase
-initializeApp(firebaseConfig);
+// //initialize firebase
+// initializeApp(firebaseConfig);
 
-//init services
-const db = getFirestore()
+// //init services
+// const db = getFirestore()
 
-// collection reference
-const colRef = collection(db, 'tasks')
+// // collection reference
+// const colRef = collection(db, 'tasks')
 
-//get collection data
-getDocs(colRef)
-  .then((snapshot) => {
-    let tasks = []
-    snapshot.docs.forEach((doc) => {
-        tasks.push({... doc.data(), id: doc.id })
-    })
-    console.log(tasks)
-  })
-  .catch(err => {
-    console.log(err.message)
-  })
-//adding a task
-const addTask = document.quirySelector('.add')
-addBookForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-
-})
-//deleting a task
+// //get collection data
+// getDocs(colRef)
+//   .then((snapshot) => {
+//     let tasks = []
+//     snapshot.docs.forEach((doc) => {
+//         tasks.push({... doc.data(), id: doc.id })
+//     })
+//     console.log(tasks)
+//   })
+//   .catch(err => {
+//     console.log(err.message)
+//   })
+// //adding a task
+// const addTask = document.quirySelector('.add')
+// addBookForm.addEventListener('submit', (e) => {
+//     e.preventDefault()
 
 
+// })
+// //deleting a task
 
 
 
@@ -59,11 +57,24 @@ addBookForm.addEventListener('submit', (e) => {
 
 
 
+
+// const Add = document.getElementById("Add");
 // const login = document.getElementById("login");
 // const signup = document.getElementById("signup");
 // const about = document.getElementById("about");
-// const Box = document.getElementById("Box");
-// const Add = document.getElementById("Add");
+const Box = document.getElementById("Box");
+let t1 = document.getElementById("t1");
+
+function addTask(){
+    if (Box.value === ''){
+        alert("please type-in a task value");
+    }
+    else{
+        t1.value = Box.value;
+        t1.innerHTML = Box.value;
+        console.log(t1);
+    }
+}
 // const pri = document.getElementById("pri");
 // const low = document.getElementById("low");
 // const medium = document.getElementById("medium");
