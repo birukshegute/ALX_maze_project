cf649434641aa8822c25c09da0a43a34c5f71c88
birukshegute <birukshegute@gmail.com>
2024-05-20 16:19:27 +0300

    a

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,47 +1,48 @@
-import { initializeApp } from "firebase/app";
-import {
-    getFirestore, collection, getDocs
-} from 'firebase/firestore'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD0AyE0wzp7Ga8BUWN_h8_Aj8qG0sLAREg",
-    authDomain: "todolist-aec4c.firebaseapp.com",
-    projectId: "todolist-aec4c",
-    storageBucket: "todolist-aec4c.appspot.com",
-    messagingSenderId: "60401679256",
-    appId: "1:60401679256:web:4026ff726358d9e6d889ed",
-    measurementId: "G-CRTYC1H96P"
-  };
+  apiKey: "AIzaSyD0AyE0wzp7Ga8BUWN_h8_Aj8qG0sLAREg",
+  authDomain: "todolist-aec4c.firebaseapp.com",
+  projectId: "todolist-aec4c",
+  storageBucket: "todolist-aec4c.appspot.com",
+  messagingSenderId: "60401679256",
+  appId: "1:60401679256:web:4026ff726358d9e6d889ed",
+  measurementId: "G-CRTYC1H96P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 //initialize firebase
 initializeApp(firebaseConfig);
 
-//init services
-const db = getFirestore()
+// //init services
+// const db = getFirestore();
 
-// collection reference
-const colRef = collection(db, 'tasks')
+// // collection reference
+// const colRef = collection(db, 'tasks')
 
-//get collection data
-getDocs(colRef)
-  .then((snapshot) => {
-    let tasks = []
-    snapshot.docs.forEach((doc) => {
-        tasks.push({... doc.data(), id: doc.id })
-    })
-    console.log(tasks)
-  })
-  .catch(err => {
-    console.log(err.message)
-  })
-//adding a task
-const addTask = document.quirySelector('.add')
-addBookForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-})
-//deleting a task
+// //get collection data
+// getDocs(colRef)
+//   .then((snapshot) => {
+//     let tasks = []
+//     snapshot.docs.forEach((doc) => {
+//         tasks.push({... doc.data(), id: doc.id })
+//     })
+//     console.log(tasks)
+//   })
+//   .catch(err => {
+//     console.log(err.message)
+//   })
+// console.log(firebase);
+
 
 
 function warning(){
-    alert("Please login to add tasks!");
+  alert("Please login to add tasks!");
 }
