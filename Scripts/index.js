209984a5621609c61ba57fcdd0fb695f470a0355
birukshegute <birukshegute@gmail.com>
2024-05-20 16:52:53 +0300

    Don't know what to do

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,48 +1,43 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD0AyE0wzp7Ga8BUWN_h8_Aj8qG0sLAREg",
-  authDomain: "todolist-aec4c.firebaseapp.com",
-  projectId: "todolist-aec4c",
-  storageBucket: "todolist-aec4c.appspot.com",
-  messagingSenderId: "60401679256",
-  appId: "1:60401679256:web:4026ff726358d9e6d889ed",
-  measurementId: "G-CRTYC1H96P"
-};
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD0AyE0wzp7Ga8BUWN_h8_Aj8qG0sLAREg",
+//   authDomain: "todolist-aec4c.firebaseapp.com",
+//   projectId: "todolist-aec4c",
+//   storageBucket: "todolist-aec4c.appspot.com",
+//   messagingSenderId: "60401679256",
+//   appId: "1:60401679256:web:4026ff726358d9e6d889ed",
+//   measurementId: "G-CRTYC1H96P"
+// };
 
-//initialize firebase
-initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-// //init services
-// const db = getFirestore();
+// //initialize firebase
+// initializeApp(firebaseConfig);
 
-// // collection reference
-// const colRef = collection(db, 'tasks')
+// // //init services
+// // const db = getFirestore();
 
-// //get collection data
-// getDocs(colRef)
-//   .then((snapshot) => {
-//     let tasks = []
-//     snapshot.docs.forEach((doc) => {
-//         tasks.push({... doc.data(), id: doc.id })
-//     })
-//     console.log(tasks)
-//   })
-//   .catch(err => {
-//     console.log(err.message)
-//   })
-// console.log(firebase);
+// // // collection reference
+// // const colRef = collection(db, 'tasks')
 
-
-
-function warning(){
-  alert("Please login to add tasks!");
-}
+// // //get collection data
+// // getDocs(colRef)
+// //   .then((snapshot) => {
+// //     let tasks = []
+// //     snapshot.docs.forEach((doc) => {
+// //         tasks.push({... doc.data(), id: doc.id })
+// //     })
+// //     console.log(tasks)
+// //   })
+// //   .catch(err => {
+// //     console.log(err.message)
+// //   })
+// // console.log(firebase);
