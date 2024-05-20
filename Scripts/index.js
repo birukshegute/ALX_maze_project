2af0094632d35e1b6ee9f0ffb23ad619c1c0d859
birukshegute <birukshegute@gmail.com>
2024-05-20 10:29:22 +0300

    creted userpage

--- Scripts/index.js
+++ Scripts/index.js
@@ -38,54 +38,5 @@ getDocs(colRef)
 const addTask = document.quirySelector('.add')
 addBookForm.addEventListener('submit', (e) => {
     e.preventDefault()
-
-
 })
 //deleting a task
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Add = document.getElementById("Add");
-// const login = document.getElementById("login");
-// const signup = document.getElementById("signup");
-// const about = document.getElementById("about");
-const Box = document.getElementById("Box");
-const check = document.getElementById("check");
-
-function addTask(){
-    if (Box.value === ''){
-        alert("please type-in a task value");
-    }
-    else{
-        let checkbox = document.createElement("input");
-        let li = document.createElement("li");
-        let del = document.createElement("button");
-        let edit = document.createElement("button");
-        let br = document.createElement("br");
-        checkbox.type = 'checkbox';
-        li.innerHTML = Box.value;
-        del.innerHTML = '<img src="../images/delete.png">'; 
-        edit.innerHTML = '<img src="../images/edit.png">';
-        check.appendChild(li);
-        check.appendChild(del);
-        check.appendChild(edit);
-        check.appendChild(br)
-    }
-}
-// const pri = document.getElementById("pri");
-// const low = document.getElementById("low");
-// const medium = document.getElementById("medium");
-// const high = document.document.getElementById("high");
