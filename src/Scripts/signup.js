f8109a28f6d1387957593a3c10c7ec0197d0e0e5
birukshegute <birukshegute@gmail.com>
2024-05-25 00:13:41 +0300

    trying to git ignore .firebase

--- src/Scripts/signup.js
+++ src/Scripts/signup.js
@@ -1,6 +1,6 @@
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
- import { getAuth, createUserWithEmailAndPassword } from "/__/firebase/10.5.0/firebase-auth-compat.js";
+ import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebase/10.5.0/firebase-auth-compat.js";
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
